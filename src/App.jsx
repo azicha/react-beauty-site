f356685a5cdf5be2fba5961fae19662d525e10e0
azicha <askakauchi@gmail.com>
2023-05-16 02:06:48 +0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Button from '@mui/material/Button';
+
+export default function App() {
+    return (
+        <div>
+            <h1 className="text-2xl">Hello World</h1>
+            <Button variant="contained">Hello World</Button>
+        </div>
+    )
+}
