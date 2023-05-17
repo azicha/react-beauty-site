52b8dcfe9bad50212df207678075e7269367d355
azicha <askakauchi@gmail.com>
2023-05-17 15:02:47 +0300

    Create package to hold app data

---
+++ src/data/manicure-items.js
@@ -0,0 +1,80 @@
+export default [
+    {
+        id: 1,
+        itemName: "Sample Name A",
+        itemImageLink: "https://files.fm/thumb_show.php?i=a3shwcrwr",
+        itemDescription: "lorem"
+    },
+    {
+        id: 2,
+        itemName: "Sample Name B",
+        itemImageLink: "https://files.fm/thumb_show.php?i=a3shwcrwr",
+        itemDescription: "lorem"
+    },
+    {
+        id: 3,
+        itemName: "Sample Name C",
+        itemImageLink: "https://files.fm/thumb_show.php?i=a3shwcrwr",
+        itemDescription: "lorem"
+    },
+    {
+        id: 4,
+        itemName: "Sample Name D",
+        itemImageLink: "https://files.fm/thumb_show.php?i=a3shwcrwr",
+        itemDescription: "lorem"
+    },
+    {
+        id: 5,
+        itemName: "Sample Name E",
+        itemImageLink: "https://files.fm/thumb_show.php?i=a3shwcrwr",
+        itemDescription: "lorem"
+    },
+    {
+        id: 6,
+        itemName: "Sample Name F",
+        itemImageLink: "https://files.fm/thumb_show.php?i=a3shwcrwr",
+        itemDescription: "lorem"
+    },
+    {
+        id: 7,
+        itemName: "Sample Name G",
+        itemImageLink: "https://files.fm/thumb_show.php?i=a3shwcrwr",
+        itemDescription: "lorem"
+    },
+    {
+        id: 8,
+        itemName: "Sample Name H",
+        itemImageLink: "https://files.fm/thumb_show.php?i=a3shwcrwr",
+        itemDescription: "lorem"
+    },
+    {
+        id: 9,
+        itemName: "Sample Name I",
+        itemImageLink: "https://files.fm/thumb_show.php?i=a3shwcrwr",
+        itemDescription: "lorem"
+    },
+    {
+        id: 10,
+        itemName: "Sample Name J",
+        itemImageLink: "https://files.fm/thumb_show.php?i=a3shwcrwr",
+        itemDescription: "lorem"
+    },
+    {
+        id: 11,
+        itemName: "Sample Name K",
+        itemImageLink: "https://files.fm/thumb_show.php?i=a3shwcrwr",
+        itemDescription: "lorem"
+    },
+    {
+        id: 12,
+        itemName: "Sample Name L",
+        itemImageLink: "https://files.fm/thumb_show.php?i=a3shwcrwr",
+        itemDescription: "lorem"
+    },
+    {
+        id: 13,
+        itemName: "Sample Name M",
+        itemImageLink: "https://files.fm/thumb_show.php?i=a3shwcrwr",
+        itemDescription: "lorem"
+    },
+];
