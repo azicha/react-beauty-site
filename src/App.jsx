161140849a68eebbccd33803af352d0c7d16039b
azicha <askakauchi@gmail.com>
2023-05-25 15:02:54 +0300

    Creating Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-import React from "react";
-import Button from '@mui/material/Button';
+import React, { useState, useEffect } from "react";
 import NavigationComp from "./components/NavigationComp";
+import HomeViews from "./views/HomeViews";
+import ManicureViews from "./views/ManicureViews";
+import PedicureViews from "./views/PedicureViews";
+import HairViews from "./views/HairViews";
+import FacialsViews from "./views/FacialsViews";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 
 export default function App() {
     return (
-        <div className="h-screen">
-            <NavigationComp />
-            <div className="h-[92%]">
-            </div>
+        <div>
+            <NavigationComp  />
+            <Router>
+                <Routes>
+                    <Route path="/" element={ <HomeViews /> } />          
+                    <Route path="/manicure" element={ <ManicureViews /> } />          
+                    <Route path="/facial" element={ <FacialsViews /> } />          
+                    <Route path="/hair" element={ <HairViews /> } />          
+                    <Route path="/pedicure" element={ <PedicureViews/> } />          
+                             
+                </Routes>
+            </Router>
         </div>
     )
 }
