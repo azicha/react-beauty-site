20e9be2690ed25a7eba41f8fa0c45f27e0e206db
azicha <askakauchi@gmail.com>
2023-05-25 15:06:01 +0300

    Code Refactoring

--- src/views/HairViews.jsx
+++ src/views/HairViews.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import ProductDisplayComp from "../components/ProductDisplayComp";
+import hairItems from "../data/hair-items";
+//import SideBarComp from "../components/SideBarComp"
 
 export default function HairViews() {
+    const hairItemsDisplay = hairItems.map((item) => {
+        return(
+            <ProductDisplayComp
+                key={ item.id }
+                itemImageLink={ item.itemImageLink }
+                itemDescription={ item.itemDescription } />
+        );
+    });
+
     return (
-        <div>
-            <h1>Hello World</h1>
+        <div  className="h-full grid justify-items-stretch m-3 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {/* <SideBarComp /> */}
+        { hairItemsDisplay }
+            
         </div>
     )
 }
--- src/views/PedicureViews.jsx
+++ src/views/PedicureViews.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import ProductDisplayComp from "../components/ProductDisplayComp";
+import manicureItems from "../data/manicure-items";
 
-export default function PedicureViews() {
+export default function HomeViews() {
+    const maniCureItemsDisplay = manicureItems.map((item) => {
+        return(
+            <ProductDisplayComp
+                key={ item.id }
+                itemName={ item.itemName }
+                itemImageLink={ item.itemImageLink }
+                itemDescription={ item.itemDescription } />
+        );
+    });
+
     return (
-        <div>
-            <h1>Hello World</h1>
+        <div className="h-full grid justify-items-stretch m-3 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            { maniCureItemsDisplay }
         </div>
     )
 }
