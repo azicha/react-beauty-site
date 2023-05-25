7ed5973fd84a51959c91867556eabdae269cf791
azicha <askakauchi@gmail.com>
2023-05-25 14:59:28 +0300

    Creatin Hair items list

---
+++ src/data/hair-items.js
@@ -0,0 +1,67 @@
+export default [
+    {
+        id: 1,
+        itemImageLink: "https://files.fm/thumb_show.php?i=7t4htx9bn",
+        itemDescription: "MDL 4*4 Straight Short Bob Wig With Baby Hair 8 Inch Lace Front Pre Plucked Hairline Natural Black"
+    },
+    {
+        id: 2,
+        itemImageLink: "https://files.fm/thumb_show.php?i=443jfaqpn",
+        itemDescription: "QTHAIR 12A Straight Human Hair Bundles (18 20 22, 300g, Natural Black) 100% Unprocessed Human Hair"
+    },
+    {
+        id: 3,
+        itemImageLink: "https://files.fm/thumb_show.php?i=aj78txuz8",
+        itemDescription: "Malaysian Weave Bundles Straight Hair 20 22 24 26 Inch 9A Virgin Remy Straight Hair 4 Bundles 100%"
+    },
+    {
+        id: 4,
+        itemImageLink: "https://files.fm/thumb_show.php?i=4n8dck3we",
+        itemDescription: "Looose Wave 3 Bundles with Closure Human Hair Bundles 8A Brazilizn Virgin Human Hair For Black Women"
+    },
+    {
+        id: 5,
+        itemImageLink: "https://files.fm/thumb_show.php?i=6y3q67shv",
+        itemDescription: "Body Wave Lace Front Wigs Human Hair, 13*4 Lace Frontal Human Hair Wigs for Black Women, 150% Density "
+    },
+    {
+        id: 6,
+        itemImageLink: "https://files.fm/thumb_show.php?i=ppdnmekkb",
+        itemDescription: "Amella Hair Brazilian Virgin Curly Hair 3 Bundles Hair Extensions 8A Brazilian Kinky Curly Hair"
+    },
+    {
+        id: 7,
+        itemImageLink: "https://files.fm/thumb_show.php?i=wshgv7ee9",
+        itemDescription: "10A Brazilian Hair Weave Loose Deep Wave Human Hair Bundles with Closure (24 26 28 30+20 Closure)"
+    },
+    {
+        id: 8,
+        itemImageLink: "https://files.fm/thumb_show.php?i=wkv8hnjgv",
+        itemDescription: "Longjia Show 12inch Bob Wig Human Hair 4*4 Middles Part Closure Bob Wigs Human Hair"
+    },
+    {
+        id: 9,
+        itemImageLink: "https://files.fm/thumb_show.php?i=5q3nfwt56",
+        itemDescription: "BLACKMOON HAIR Brazilian Straight Hair 3 Bundles 8 8 8inch Unprocessed Virgin Brazilian Hair"
+    },
+    {
+        id: 10,
+        itemImageLink: "https://files.fm/thumb_show.php?i=pum85f9mq",
+        itemDescription: "Bestsojoy Brazilian Virgin Hair Body Wave 4 Bundles 10A Cheap Brazilian Human Hair Weave Bundles "
+    },
+    {
+        id: 11,
+        itemImageLink: "https://files.fm/thumb_show.php?i=4zwquq7a5",
+        itemDescription: "Ombre Pre-stretched Braiding Hair, Top Silky Braid Hair Extensions, Itch Free Crotchet Twist Hair Braids"
+    },
+    {
+        id: 12,
+        itemImageLink: "https://files.fm/thumb_show.php?i=nxs4j4dp7",
+        itemDescription: "Original Jumbo Braids Hair Extension 3pcs Pure Brown Color 24inch 100g/pc For Twist Box Braiding"
+    },
+    {
+        id: 13,
+        itemImageLink: "https://files.fm/thumb_show.php?i=dp8tun7ys",
+        itemDescription: "20 Inch 8 roots Loose Wave Curly Braids Synthetic Hair Extensions For Black Women French Curls"
+    },
+];
