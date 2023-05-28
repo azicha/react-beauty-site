55b0139080546a3f89b75a1a6973bdc2fa869e08
azicha <askakauchi@gmail.com>
2023-05-28 23:46:00 +0300

    Code Refactoring

--- src/views/HomeViews.jsx
+++ src/views/HomeViews.jsx
@@ -1,21 +1,23 @@
 import React from "react";
-import ProductDisplayComp from "../components/ProductDisplayComp";
-import manicureItems from "../data/manicure-items";
+// import ProductDisplayComp from "../components/ProductDisplayComp";
+// import manicureItems from "../data/manicure-items";
 
 export default function HomeViews() {
-    const maniCureItemsDisplay = manicureItems.map((item) => {
-        return(
-            <ProductDisplayComp
-                key={ item.id }
-                itemName={ item.itemName }
-                itemImageLink={ item.itemImageLink }
-                itemDescription={ item.itemDescription } />
-        );
-    });
+    // const maniCureItemsDisplay = manicureItems.map((item) => {
+    //     return(
+    //         <ProductDisplayComp
+    //             key={ item.id }
+    //             itemName={ item.itemName }
+    //             itemImageLink={ item.itemImageLink }
+    //             itemDescription={ item.itemDescription } />
+    //     );
+    // });
 
     return (
-        <div className="h-full grid justify-items-stretch m-3 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-            { maniCureItemsDisplay }
+        <div className="bg-gray-400 h-screen  flex flex-col">
+            {/* { maniCureItemsDisplay } */}
+            <h1 className="text-lg font-bold text-center">Welcome Home</h1>
+            <h2 className="text-lg font-bold text-center">Shop With Us</h2>
         </div>
     )
 }
