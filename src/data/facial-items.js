d07ff675304165e4390cc58a656d04d818158ec1
azicha <askakauchi@gmail.com>
2023-05-28 23:47:26 +0300

    Adding Facial items

--- src/data/facial-items.js
+++ src/data/facial-items.js
@@ -1,67 +1,67 @@
 export default [
     {
         id: 1,
-        itemImageLink: "",
-        itemDescription: ""
+        itemImageLink: "https://files.fm/thumb_show.php?i=d2k3zbw89",
+        itemDescription: "CeraVe Face Wash Acne Treatment | Clogged Pore Control"
     },
     {
         id: 2,
-        itemImageLink: "",
-        itemDescription: ""
+        itemImageLink: "https://files.fm/thumb_show.php?i=9dquh483j",
+        itemDescription: "Garnier Vitamin C* Moisturizing Brightening Day Cream with Super Citrus"
     },
     {
         id: 3,
-        itemImageLink: "",
-        itemDescription: ""
+        itemImageLink: "https://files.fm/thumb_show.php?i=pvhq84npw",
+        itemDescription: "Garnier Skinactive Clearly Brighter Dark Spot Corrector with Vitamin C"
     },
     {
         id: 4,
-        itemImageLink: "",
-        itemDescription: ""
+        itemImageLink: "https://files.fm/thumb_show.php?i=yce9x8sed",
+        itemDescription: "Hydrating Face Moisturizer with Avocado Sunflower Seed Oils, Dry to Normal Skin"
     },
     {
         id: 5,
-        itemImageLink: "",
-        itemDescription: ""
+        itemImageLink: "https://files.fm/thumb_show.php?i=z4e2xx8zn",
+        itemDescription: "AZURE Collagen & Vitamin C Anti Aging Eye Serum - Toning & De-puffing Serum"
     },
     {
         id: 6,
-        itemImageLink: "",
-        itemDescription: ""
+        itemImageLink: "https://files.fm/thumb_show.php?i=nexrz49e2",
+        itemDescription: "Facial Cleansing Brush Face Scrubber: Electric Exfoliating Spin Cleanser "
     },
     {
         id: 7,
-        itemImageLink: "",
-        itemDescription: ""
+        itemImageLink: "https://files.fm/thumb_show.php?i=hk3pe8frz",
+        itemDescription: "Dr Rashel Vitamin C Face Serum And Vitamin C Face Cream Variety Pack "
     },
     {
         id: 8,
-        itemImageLink: "",
-        itemDescription: ""
+        itemImageLink: "https://files.fm/thumb_show.php?i=7k9j3az5w",
+        itemDescription: "Garnier SkinActive Face Wash with Aloe Juice, For Dry Skin"
     },
     {
         id: 9,
-        itemImageLink: "",
-        itemDescription: ""
+        itemImageLink: "https://files.fm/thumb_show.php?i=z29ay36se",
+        itemDescription: "CeraVe Foaming Facial and Hydrating Cleanser, 24 Fluid Ounce by CeraVe"
     },
     {
         id: 10,
-        itemImageLink: "",
-        itemDescription: ""
+        itemImageLink: "https://files.fm/thumb_show.php?i=th48qkszj",
+        itemDescription: "PopSockets Lip Balm Grip with Expanding Kickstand, PopLips"
     },
     {
         id: 11,
-        itemImageLink: "",
-        itemDescription: ""
+        itemImageLink: "https://files.fm/thumb_show.php?i=5kwdeyqcx",
+        itemDescription: "Generic Glossy Lips Lip Glosses in Crystal Clear or Shimmering Glitter"
     },
     {
         id: 12,
-        itemImageLink: "",
-        itemDescription: ""
+        itemImageLink: "https://files.fm/thumb_show.php?i=f2d7qja98",
+        itemDescription: "CeraVe Face Sunscreen with Zinc Oxide & Titanium Oxide for Sensitive Skin"
     },
     {
         id: 13,
-        itemImageLink: "",
-        itemDescription: ""
+        itemImageLink: "https://files.fm/thumb_show.php?i=skc69a2uf",
+        itemDescription: "Garnier SkinActive 3-in-1 Face Moisturizer with Green Tea, Oily Skin, 6.75 fl. oz."
     }
 ]
