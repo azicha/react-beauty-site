dd8750771ac8d5d94310e2d4a3ed269107a07680
azicha <askakauchi@gmail.com>
2023-05-28 17:06:40 +0300

    Code Refactoring

--- src/views/PedicureViews.jsx
+++ src/views/PedicureViews.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ProductDisplayComp from "../components/ProductDisplayComp";
-import manicureItems from "../data/manicure-items";
+import pedicureItems from "../data/pedicure-items";
 
 export default function HomeViews() {
-    const maniCureItemsDisplay = manicureItems.map((item) => {
+    const pedicureItemsDisplay = pedicureItems.map((item) => {
         return(
             <ProductDisplayComp
                 key={ item.id }
@@ -15,7 +15,7 @@ export default function HomeViews() {
 
     return (
         <div className="h-full grid justify-items-stretch m-3 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-            { maniCureItemsDisplay }
+            { pedicureItemsDisplay }
         </div>
     )
 }
