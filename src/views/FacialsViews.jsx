4a1148052b03124b47d0c332d1a3959c04b533ba
azicha <askakauchi@gmail.com>
2023-05-28 23:46:43 +0300

    Code Refactoring

--- src/views/FacialsViews.jsx
+++ src/views/FacialsViews.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ProductDisplayComp from "../components/ProductDisplayComp";
-import manicureItems from "../data/manicure-items";
+import facialItems from "../data/facial-items";
 
 export default function HomeViews() {
-    const maniCureItemsDisplay = manicureItems.map((item) => {
+    const facialItemsDisplay = facialItems.map((item) => {
         return(
             <ProductDisplayComp
                 key={ item.id }
@@ -15,7 +15,7 @@ export default function HomeViews() {
 
     return (
         <div className="h-full grid justify-items-stretch m-3 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-            { maniCureItemsDisplay }
+            { facialItemsDisplay }
         </div>
     )
 }
