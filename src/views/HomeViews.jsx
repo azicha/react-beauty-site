97d8c735917e1af343192f78a312af93dd3715ad
azicha <askakauchi@gmail.com>
2023-06-05 15:17:34 +0300

    Creating home view

--- src/views/HomeViews.jsx
+++ src/views/HomeViews.jsx
@@ -1,24 +1,12 @@
 import React from "react";
-// import ProductDisplayComp from "../components/ProductDisplayComp";
-// import manicureItems from "../data/manicure-items";
-
 
 export default function HomeViews() {
-    // const maniCureItemsDisplay = manicureItems.map((item) => {
-    //     return(
-    //         <ProductDisplayComp
-    //             key={ item.id }
-    //             itemName={ item.itemName }
-    //             itemImageLink={ item.itemImageLink }
-    //             itemDescription={ item.itemDescription } />
-    //     );
-    // });
-
     return (
-        <div className="bg-gray-400 h-screen  flex flex-col">
-            {/* { maniCureItemsDisplay } */}
-            <h1 className="text-lg font-bold text-center">Welcome Home</h1>
-            <h2 className="text-lg font-bold text-center">Shop With Us</h2>
+        <div className="w-full flex flex-col justify-center h-screen p-4">
+           <p className="mb-2">WELCOME TO AZICHA BEAUTY SHOP</p>
+           <h1 className="font-bold text-5xl mb-2">Brightening homes with beautiful, durable products</h1>
+           {/* <Button variant="contained" className="w-fit">Download CV</Button> */}
         </div>
+            
     )
 }
