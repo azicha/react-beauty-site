56f465ce5b252ffa60c9e70826bd748f8dfc07b2
azicha <askakauchi@gmail.com>
2023-06-08 15:46:59 +0300

    Code Refctoring

--- src/views/HomeViews.jsx
+++ src/views/HomeViews.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function HomeViews() {
     return (
-        <div className="w-full flex flex-col justify-center h-screen p-4">
-           <p className="mb-2">WELCOME TO AZICHA BEAUTY SHOP</p>
-           <h1 className="font-bold text-5xl mb-2">Brightening homes with beautiful, durable products</h1>
-           {/* <Button variant="contained" className="w-fit">Download CV</Button> */}
+        <div className="h-screen flex bg-slate-100">
+            <div className="flex flex-col justify-center p-4 h-full w-3/4">
+                <p className="mb-2 text-5xl font-semibold">AZICHA BEAUTY SHOP</p>
+                <h1 className="text-3xl mb-2">Brightening homes with beautiful, durable products.</h1>
+                <div className="border-2 w-[15%] p-4 pl-10 rounded-md ml-8 shadow-lg mt-2 bg-yellow-800">
+                <Link to="/facial" className="text-lg">SHOP NOW</Link>
+                </div>
+            </div>
+            <div className="flex justify-end items-center h-full w-1/4">
+                <img src="https://files.fm/thumb_show.php?i=cp2p8hvgy" alt="homepage-image" className="h-[100%] w-full object-cover mr-10" />
+            </div>
         </div>
             
     )
